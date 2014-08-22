484209b3056c83093a843e892e64e46052508ae4
siwangqishiq <525647740@qq.com>
2014-08-22 15:34:47 +0800

    中缀表达式 转化为 后缀表达式(逆波兰式)

--- DataStruct/src/com/xinlan/datastruct/stack/Expression.java
+++ DataStruct/src/com/xinlan/datastruct/stack/Expression.java
@@ -1,7 +1,10 @@
 package com.xinlan.datastruct.stack;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * 表达式处理类
@@ -12,26 +15,110 @@ import java.util.List;
 public class Expression
 {
     private String data;
-    private List<Object> store;
+    private List<Object> storeMidfix;
+    private List<Object> postfix;//
+
+    private static Map<Character, Integer> priority = new HashMap<Character, Integer>();
+    static
+    {
+        priority.put('+', 1);
+        priority.put('-', 1);
+        priority.put('*', 3);
+        priority.put('/', 3);
+        priority.put('(', 0);
+        priority.put(')', 0);
+    }
 
     public Expression(String data)
     {
         this.data = data;
     }
 
+    /**
+     * 中缀表达式改写为后缀表达式 逆波兰式
+     * 
+     * @return
+     */
     public String toPostfix()
     {
-        store = splitExpress();
+        storeMidfix = splitExpress();
 
-        for (int i = 0; i < store.size(); i++)
+        System.out.println("-------表达式-----");
+        for (int i = 0; i < storeMidfix.size(); i++)
         {
-            System.out.print(store.get(i).toString() + " ");
-        }
-        
-        
+            System.out.print(storeMidfix.get(i).toString() + " ");
+        }// end for i
+        System.out.println();
 
-        return "";
+        postfix = new ArrayList<Object>();
+        // postfix.addAll(storeMidfix);
+        Stack<Object> workStack = new Stack<Object>();
+        for (int i = 0, size = storeMidfix.size(); i < size; i++)
+        {
+            Object element = storeMidfix.get(i);
+            if (element instanceof String)
+            {
+                postfix.add(element);
+            }
+            else if (element instanceof Character)
+            {
+                Character curElement = (Character) element;
+                switch (curElement)
+                {
+                    case '(':
+                        workStack.push(curElement);// 操作符 ( 入栈
+                        break;
+                    case ')':
+                        while (!workStack.isEmpty()
+                                && (Character) workStack.peek() != '(')
+                        {
+                            Character addOutput = (Character) workStack.pop();// 当前栈顶元素出栈
+                            if (addOutput != '(')// 运算符 加入输出表
+                            {
+                                postfix.add(addOutput);// 加入输出列表
+                            }
+                        }// end while
+                        
+                        if((Character) workStack.peek() == '(')
+                        {
+                            workStack.pop();//移除栈中的'('字符
+                        }
+                        break;
+                    default:
+                        while (!workStack.isEmpty()
+                                && priority.get((Character) workStack.peek()) >= priority
+                                        .get(curElement))
+                        {
+                            Character addOutput = (Character) workStack.pop();// 当前栈顶元素出栈
+                            postfix.add(addOutput);// 加入输出列表
+                        }// end while
+                        workStack.push(curElement);// 当前操作符 入栈
+                }// end switch
+            }// end if
+        }// end for i
+
+        while (!workStack.isEmpty())
+        {
+            Character addToOutput = (Character) workStack.pop();// 当前栈顶元素出栈
+            if (addToOutput != '(')// 不记录 字符'('
+            {
+                postfix.add(addToOutput);// 加入输出列表
+            }
+        }// end while
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < postfix.size(); i++)
+        {
+            // System.out.print(postfix.get(i).toString() + " ");
+            sb.append(postfix.get(i).toString() + "  ");
+        }// end for i
+        System.out.println("-------后缀表达式-----");
+        System.out.println(sb.toString());
+
+        return sb.toString();
     }
+    
+    
 
     private List<Object> splitExpress()
     {
@@ -42,12 +129,15 @@ public class Expression
         for (int i = 0, length = dataChars.length; i < length; i++)
         {
             char oneChar = dataChars[i];
+
             switch (oneChar)
             {
                 case '+':
                 case '-':
                 case '*':
                 case '/':
+                case '(':
+                case ')':
                     retList.add(temp);
                     temp = "";
                     retList.add(oneChar);
--- DataStruct/src/com/xinlan/datastruct/stack/ExpressionTest.java
+++ DataStruct/src/com/xinlan/datastruct/stack/ExpressionTest.java
@@ -5,7 +5,8 @@ public class ExpressionTest
 
     public static void main(String[] args)
     {
-        Expression s = new Expression(" 23 + 57 ");
+        Expression s = new Expression(" 23 + 57*(3 +100)+200 ");
+        //Expression s = new Expression(" a*(b+c)+d");
         s.toPostfix();
     }
 
